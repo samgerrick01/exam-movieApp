7f85b77bd0be894ed1930c222b1551f0f92a857a
SamGerrick <desilva.sam17.sgds@gmail.com>
2024-05-01 20:16:34 +0800

    Install and set up the fonts needed

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,29 +1,25 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
+import {
+  Poppins_400Regular,
+  Poppins_700Bold,
+} from '@expo-google-fonts/poppins';
+import { Sora_400Regular, Sora_700Bold } from '@expo-google-fonts/sora';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 
-export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
-} from 'expo-router';
+export { ErrorBoundary } from 'expo-router';
 
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
-};
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
+    Sora: Sora_400Regular,
+    SoraBold: Sora_700Bold,
+    Poppins: Poppins_400Regular,
+    PoppinsBold: Poppins_700Bold,
   });
 
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
   useEffect(() => {
     if (error) throw error;
   }, [error]);
