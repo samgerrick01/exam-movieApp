bb8de752aed2710cd932ef9130ad7e789dd64543
SamGerrick <desilva.sam17.sgds@gmail.com>
2024-05-01 22:06:51 +0800

    Add assets and make a welcome screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { useSelector } from 'react-redux';
@@ -8,9 +8,36 @@ const index = () => {
   const { movies } = useSelector((state: RootState) => state.movie);
 
   return (
-    <View className='flex-1 items-center justify-center bg-white'>
+    <View className='flex-1 justify-end items-center space-y10 relative'>
       <StatusBar style='auto' hidden />
-      <Text className=' text-red-500'>Test App</Text>
+      <Image
+        source={require('@assets/images/welcome.png')}
+        className=' w-full h-full absolute'
+        resizeMode='cover'
+      />
+      {/* Title and Button */}
+      <View className='flex justify-center items-center py-28 max-w-[80%]'>
+        <View className='bg-red-600 p-4 rounded-3xl'>
+          <Text className='text-white text-4xl font-extrabold tracking-widest my-4'>
+            DXB
+          </Text>
+        </View>
+        <Text className='text-white text-4xl font-bold tracking-widest my-4'>
+          Movie App
+        </Text>
+
+        <Text className='text-white tracking-widest mb-2 text-lg text-center font-medium'>
+          Watch and find movies that bring your mood back.
+        </Text>
+      </View>
+      <View className='my-4 mb-36'>
+        <TouchableOpacity
+          className='px-12 py-3 rounded-lg bg-red-600'
+          onPress={() => {}}
+        >
+          <Text className='text-white text-lg font-medium'>Explore</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
