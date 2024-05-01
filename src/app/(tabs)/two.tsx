aa0dde470a18905a8fe61758a223f71fa63b8f79
SamGerrick <desilva.sam17.sgds@gmail.com>
2024-05-01 19:58:48 +0800

    first commit

---
+++ src/app/(tabs)/two.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const two = () => {
+  return (
+    <View>
+      <Text>two</Text>
+    </View>
+  );
+};
+
+export default two;
+
+const styles = StyleSheet.create({});
